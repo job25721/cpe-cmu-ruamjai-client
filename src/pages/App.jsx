97c583efdebf658964cba194c557871131b83bf0
wongwhale <wongwhale_@hotmail.com>
2020-09-28 16:33:10 +0700

    status

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -46,6 +46,12 @@ const App = (props) => {
         // status="รอการอนุมัติ"
         voting="100"
       />
+      <Card 
+        header="หัวข้อ"
+        detail="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti alias ratione corporis eum enim porro fugit et ad, incidunt possimus unde provident itaque beatae similique pariatur, distinctio veniam error quisquam?"
+        // status="รอการอนุมัติ"
+        voting="50"
+      />
     </>
   );
 };
