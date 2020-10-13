94d33101aeaf347c5a6d4add8fe8c2e59764dd3a
job25721 <pathomporn_pankaew@cmu.ac.th>
2020-10-13 14:31:14 +0700

    decorate auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Auth = () => {
   return (
     <div
-      className="container flex-column align-items-center justify-center"
-      style={{ height: "100vh" }}
+      className="container is-fluid flex-column align-items-center justify-center"
+      style={{ height: "100vh", background: "whitesmoke" }}
     >
       <div
         className="box"
-        style={{ boxShadow: "1px 1px 10px var(--lightPurple)" }}
+        style={{ boxShadow: "1px 1px 10px var(--lightPurple)", maxWidth: 350 }}
       >
         <h1 className="subtitle is-3">Login</h1>
         <input
